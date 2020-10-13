78fa6161d6f0d0e68d26d1b443969ca1b75a3ccb
Your Name <ga.2.y1220@gmail.com>
2020-10-13 12:39:31 +0200

    insecure the correct insertion

--- index.rb
+++ index.rb
@@ -5,15 +5,41 @@ require "./player"
 puts "player1: insert name and x or o"
 print "player1-name: "
 name= gets.chomp
-puts ""
 print "player1- x or o: "
-x_o= gets.chomp
-puts ""
-player1 = Player.new(name, x_o)
+x_o1= gets.chomp
+while x_o1!='x' && x_o1!='o'
+	puts "please insert either x or o"
+	print "player1- x or o: "
+	x_o1= gets.chomp
+end
+player1 = Player.new(name, x_o1)
 
-puts"check the insertion"
-puts "name: "+ player1.name
-puts "x or o: "+ player1.x_o
+
+
+puts "player2: insert name and x or o"
+print "player2-name: "
+name= gets.chomp
+print "player2- x or o: "
+x_o2= gets.chomp
+if x_o1=='x'
+	while x_o2!='o' 
+		puts "please insert o since x is taken by another player"
+		print "player2- x or o: "
+		x_o2= gets.chomp
+	end
+elsif x_o1=='o'
+	while x_o2!='x'
+		puts "please insert x since o is taken by another player"
+		print "player2- x or o: "
+		x_o2= gets.chomp
+	end
+end
+
+player2 = Player.new(name, x_o2)
+
+puts"=================check the insertion=========================="
+puts "player1: "+ player1.name+ " "+ player1.x_o
+puts "player2: "+ player2.name+ " "+ player2.x_o
 
 
 
