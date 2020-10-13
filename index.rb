06a7732dfce380bf338fd55cc95b430dca64d2da
Your Name <ga.2.y1220@gmail.com>
2020-10-13 13:03:13 +0200

    read the coordinates

--- index.rb
+++ index.rb
@@ -41,9 +41,35 @@ puts"=================check the insertion=========================="
 puts "player1: "+ player1.name+ " "+ player1.x_o
 puts "player2: "+ player2.name+ " "+ player2.x_o
 
+# explanation of the way to choose the position
+puts " (0,0)|(0,1)|(0,2)"
+puts "   -     -     -  "
+puts " (1,0)|(1,1)|(1,2)"
+puts "   -     -     -  "
+puts " (2,0)|(2,1)|(2,2)"
+puts "please insert the coordinate (x,y) as above"
 
 
 # read the coordinates
+puts "tern of #{player1.name}"
+print "x: "
+x1= gets.chomp.to_i
+print "y: "
+y1= gets.chomp.to_i
+
+puts "tern of #{player2.name}"
+print "x: "
+x2= gets.chomp.to_i
+print "y: "
+y2= gets.chomp.to_i
+
+puts"=================check the insertion=========================="
+puts player1.name + ": ("+ x1.to_s + ", " + y1.to_s + ")"
+puts player2.name + ": ("+ x2.to_s + ", " + y2.to_s + ")"
+
+
+
+
 
 # check whether player wins or not
 
